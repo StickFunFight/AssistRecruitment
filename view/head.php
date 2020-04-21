24d787e52fa1b9ffd36a3020cf4f97a27ea56b19
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-04-21 10:22:44 +0200

    Opzet mappen structuur

--- view/head.php
+++ view/head.php
@@ -8,11 +8,11 @@
 
     <head>
         <!--Preloading all important things-->
-        <link rel="preload" href="assests/styling/main.css" as="style">
-        <link rel="preload" href="assests/script/main.js" as="script">
-        <link rel="preload" href="assests/script/validation.js" as="script">
-        <link rel="preload" href="assests/bootstrap/css/bootstrap.min.css" as="style">
-        <link rel="preload" href="assests/bootstrap/js/bootstrap.min.js" as="script">
+        <link rel="preload" href="../assests/styling/main.css" as="style">
+        <link rel="preload" href="../assests/script/main.js" as="script">
+        <link rel="preload" href="../assests/script/validation.js" as="script">
+        <link rel="preload" href="../assests/bootstrap/css/bootstrap.min.css" as="style">
+        <link rel="preload" href="../assests/bootstrap/js/bootstrap.min.js" as="script">
 
         <!-- all meta tags -->
         <meta charset="utf-8">
@@ -20,20 +20,20 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Linking to Bootstrap CSS -->
-        <link rel="stylesheet" href="assests/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../assests/bootstrap/css/bootstrap.min.css">
 
         <!-- Linking to Bootstrap JavaScript -->
-        <script src="assests/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../assests/bootstrap/js/bootstrap.min.js"></script>
 
         <!-- Linking voor jQuery -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
         <!-- Linking to own CSS -->
-        <link rel="stylesheet" href="assests/styling/main.css">
+        <link rel="stylesheet" href="../assests/styling/main.css">
 
         <!-- Linking to own JavaScript -->
-        <script src="assests/script/validation.js"></script>
-        <script src="assests/script/main.js"></script>
+        <script src="../assests/script/validation.js"></script>
+        <script src="../assests/script/main.js"></script>
 
         <!-- Linking to icons -->
         <script src='https://kit.fontawesome.com/a076d05399.js'></script>
