52f78abe4ea4dd399473fa6fc28fbb8d53d65284
Dominique <niehau@gmail.com>
2020-04-21 15:35:38 +0200

    added logo to login screen

--- view/loginScreen.php
+++ view/loginScreen.php
@@ -3,13 +3,16 @@
 
 
 <body>
+    <div class="container">
+        <img class="logo" src="../assests/images/logoFullAssist.png" alt="Assist logo">
+    </div>
     <div class="container login-form pb-1">
         <form class="form-group">
             <input class="form-control mt-2" type="text" placeholder="Gebruikersnaam" ><br>
             <input class="form-control mt-n2" type="password" placeholder="Wachtwoord" ><br>
             <button type="submit" class="btn btn-block btn-primary mt-2 mb-4">Inloggen</button>
         </form>
-        <p class="mb-1"><u><a href="#blank" >Maak een account aan +</a></u></p>
-        <p><u><a href="#blank" >Wachtwoord vergeten?</a></u></p>
+        <p class="mb-1"><u><a href="#blank" class="font-weight-bold">Maak een account aan +</a></u></p>
+        <p><u><a href="#blank" class="font-weight-bold">Wachtwoord vergeten?</a></u></p>
     </div>
 </body>
