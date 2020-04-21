564066595932370afc9e048598d8c53a34b12e48
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-04-21 11:18:48 +0200

    Update EntCategory.php

--- functions/models/EntCategory.php
+++ functions/models/EntCategory.php
@@ -1,1 +1,26 @@
 <?php
+class EntCategory
+{
+    private $categorieID;
+    private $categorieName;
+    private $categorieStatus;
+
+    function __construct($categorieID, $categorieName, $categorieStatus)
+    {
+        $this->ID = $categorieID;
+        $this->Name = $categorieName;
+        $this->Status = $categorieStatus;
+
+    }
+    function GetID(){
+        return $this->ID;
+    }
+
+    function GetNaam(){
+        return $this->Name;
+    }
+
+    function GetStatus(){
+        return $this->Status;
+    }
+}
