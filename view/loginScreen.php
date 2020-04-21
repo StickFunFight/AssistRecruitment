78ac7c42251e30dfb09afd62948b0dc5ff388fdd
Dominique <niehau@gmail.com>
2020-04-21 12:35:44 +0200

    created login html

---
+++ view/loginScreen.php
@@ -0,0 +1,15 @@
+<?php include 'head.php'; ?>
+<style><?php include '../assests/styling/loginScreen.css' ?></style>
+
+
+<body>
+    <div class="container login-form pb-1">
+        <form class="form-group">
+            <input class="form-control mt-2" type="text" placeholder="Gebruikersnaam" ><br>
+            <input class="form-control mt-n2" type="password" placeholder="Wachtwoord" ><br>
+            <button type="submit" class="btn btn-block btn-primary mt-2 mb-4">Inloggen</button>
+        </form>
+        <p class="mb-1"><u><a href="#blank" >Maak een account aan +</a></u></p>
+        <p><u><a href="#blank" >Wachtwoord vergeten?</a></u></p>
+    </div>
+</body>
