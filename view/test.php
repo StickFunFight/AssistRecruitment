6521e79eeaee50ca8a26b2d44ef9afcf301bffa3
Marvin vissers <32897103+Booreilandtje@users.noreply.github.com>
2020-04-22 09:11:46 +0200

    Menu styling aangepast en probleem geen ruimte naast menu gefixed

---
+++ view/test.php
@@ -0,0 +1,11 @@
+<?php  
+    require('menu.php');
+?>
+<html>
+    <body>
+        <div class="iets"> hai <div> 
+
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+    </body>
+</html>
