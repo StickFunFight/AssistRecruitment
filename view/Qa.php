379b933149112adde657caba0ec05fa756a52ea6
MarvinVissers <vissers2000@gmail.com>
2020-04-24 13:05:42 +0200

    Styling goed gezet.

--- view/Qa.php
+++ view/Qa.php
@@ -1,27 +1,8 @@
-<!--<!DOCTYPE HTML>-->
-<!--<html lang="en">-->
-<!--<head>-->
-<!--    <meta name='viewport' content='width=device-width, initial-scale=1'>-->
-<!--    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
-<!--    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.20/sl-1.3.1/datatables.min.css"/>-->
-<!--    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css"/>-->
-<!--    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
-<!--    <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>-->
-<!--    <script type="text/javascript" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>-->
-<!--    <script src='https://kit.fontawesome.com/a076d05399.js'></script>-->
-<!---->
-<!--<script>-->
-<!--    $(document).ready(function() {-->
-<!--        $('#example').DataTable();-->
-<!--        $('#example2').DataTable();-->
-<!--    } );-->
-<!--</script>-->
-<!---->
-<!--</head>-->
 <?php
 require_once 'head.php';
 ?>
 <body>
+<link rel="stylesheet" href="../assests/styling/customer.css">
 <div id="wrapper">
     <div id="first">
         <table id="example2" class="table">
