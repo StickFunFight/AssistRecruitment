a124b1d35218a5109d99b674f73f87ca2d7f4d8d
Marvin vissers <32897103+MarvinVissers@users.noreply.github.com>
2020-04-24 13:59:58 +0200

    Update CustomerController.php
    Nu met Remco's functie

--- functions/controller/CustomerController.php
+++ functions/controller/CustomerController.php
@@ -21,6 +21,16 @@
         function deleteCustomers($customerID){
             $this->customerDB->deleteCustomers($customerID);
         }
+        
+        //This function creates a new customer using the values obtained from the customerDatabase (createCustomer) function.
+        //Also gives an echo as feedback to assure the user that the statement has succesfully been executed or not.
+        public function createCustomer($customerName, $customerComment, $customerReference) {
+            if($this->custAdd->createCustomer($customerName, $customerComment, $customerReference)){
+                echo "Customer succesfully added!";
+            } else {
+                echo "An error occured in the createCustomer function within the customerAdd class.";
+            }
+        }
 
         // Function to get the details of a customer
         function getCustomerDetails($customerID) {
