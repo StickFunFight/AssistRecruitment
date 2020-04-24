3780021eeafab581ca26a30a2e563b22fddc64c2
Marvin vissers <32897103+MarvinVissers@users.noreply.github.com>
2020-04-24 14:01:25 +0200

    Update CustomerDB.php
    Remco's customer opslaan functie toegevoegd

--- functions/datalayer/CustomerDB.php
+++ functions/datalayer/CustomerDB.php
@@ -36,6 +36,29 @@ function getCustomers($status){
                     echo "Er is iets fout gegaan wardoor er geen functies opgehaald konden worden";
                 }
                 }
+         
+         //Function to bind the values required to execute the createCustomer function in the customerAdd class.
+   public function createCustomer($customerName, $customerComment, $customerReference) {
+      $connection = new Database();
+      $db = $connection->getConnection();
+
+      $customerStatus = 'Active';
+
+      //A query is create here and values are being bound to the parameters inside the query.
+      $stmt = $db->prepare("INSERT INTO customer (customerID, customerName, customerComment, customerReference, customerStatus) VALUES (?, ?, ?, ?, ?)");
+      $stmt->bindParam(1, $customerID);
+      $stmt->bindParam(2, $customerName);
+      $stmt->bindParam(3, $customerComment);
+      $stmt->bindParam(4, $customerReference);
+      $stmt->bindParam(5, $customerStatus);
+
+      //The previous query statement will be executed here.
+      try {
+         return $stmt->execute();
+      } catch (PDOException $exception){
+         return false;
+      }
+   }
 
 function archiveCustomer($customerID){
 // Create Query to update Customer Status
