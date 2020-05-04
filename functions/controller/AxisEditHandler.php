fcc4b45f8beb136e3e9413d6d503d522cd1535e8
Stan <stanbegthel@gmail.com>
2020-05-04 15:32:05 +0200

    QA-Axis-Edit-v1.0
    Added pop-up with front-end and a working back-end

---
+++ functions/controller/AxisEditHandler.php
@@ -0,0 +1,20 @@
+<?php
+require '../datalayer/AxisDatabase.php';
+
+$AD = new AxisDatabase();
+
+if (isset($_POST['AxisName']) && isset($_POST['AxisStatus']) && isset($_POST['AxisID'])){
+
+    $AxisName = $_POST['AxisName'];
+    $AxisStatus = $_POST['AxisStatus'];
+    $AxisID = $_POST['AxisID'];
+
+
+
+    $AD->AxisAanpassen($AxisName, $AxisStatus, $AxisID);
+    echo "Succes";
+
+
+}else echo "failed";
+
+?>
--- functions/datalayer/AxisDatabase.php
+++ functions/datalayer/AxisDatabase.php
@@ -11,15 +11,25 @@ class AxisDatabase{
     $this->conn = $database->getConnection();
 
 }
-    public function AxesOpslaan($AxisName){
+    public function AxisOpslaan($AxisName){
 
 
         $query = "INSERT INTO axis (AxisName, AxisStatus) VALUES ('$AxisName', 'Active')";
         $stm = $this->conn->prepare($query);
         if ($stm->execute()){
-            
+
         }
 
+    }
+
+    public function AxisAanpassen($AxisName, $AxisStatus, $AxisID){
+        $query = sprintf("UPDATE axis SET AxisName = '$AxisName',
+                                       AxisStatus= '$AxisStatus'
+                                       WHERE AxisID = $AxisID");
+        $stm = $this->conn->prepare($query);
+        if ($stm->execute()){
+
+        }
     }
 
 
