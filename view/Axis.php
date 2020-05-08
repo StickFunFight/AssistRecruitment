d86487cda9c3c441c7e15aa89d016303365debbf
Stan <stanbegthel@gmail.com>
2020-05-08 14:55:57 +0200

    Demo v2.1.1
    Changed a word

--- view/Axis.php
+++ view/Axis.php
@@ -83,7 +83,7 @@ require_once 'menu.php';
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="exampleModalLabel">Categorie aanpassen</h4>
+                <h4 class="modal-title" id="exampleModalLabel">Axis aanpassen</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
