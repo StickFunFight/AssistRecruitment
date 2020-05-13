7ae82e1dbbba9516cb6a326f614859e82cdbfca0
Marvin vissers <32897103+MarvinVissers@users.noreply.github.com>
2020-05-13 13:53:18 +0200

    Sprintf veranderd voor escaped query

--- functions/datalayer/UserDB.php
+++ functions/datalayer/UserDB.php
@@ -17,15 +17,16 @@
             $listUsers = array();
 
             // Making a query to get the scans of the customer out the database
-            $query = sprintf("SELECT u.userID, c.contactID, c.contactName, c.contactPhoneNumber, u.userEmail, c.contactComment, c.contactStatus, c.contactBirth, cust.customerName, dp.departmentName, cust.customerID, dp.departmentID
-                            FROM user u
-                            INNER JOIN contact c ON c.userID = u.userID
-                            INNER JOIN customer cust ON c.customerID = cust.customerID
-                            INNER JOIN department_contact dc ON dc.contactID = c.contactID
-                            INNER JOIN department dp ON dc.departmentID = dp.departmentID
-                            WHERE c.contactStatus = '%s'
-                            ORDER BY c.contactName ASC", $status);
+            $query = "SELECT u.userID, c.contactID, c.contactName, c.contactPhoneNumber, u.userEmail, c.contactComment, c.contactStatus, c.contactBirth, cust.customerName, dp.departmentName, cust.customerID, dp.departmentID
+                      FROM user u
+                      INNER JOIN contact c ON c.userID = u.userID
+                      INNER JOIN customer cust ON c.customerID = cust.customerID
+                      INNER JOIN department_contact dc ON dc.contactID = c.contactID
+                      INNER JOIN department dp ON dc.departmentID = dp.departmentID
+                      WHERE c.contactStatus = ?
+                      ORDER BY c.contactName ASC";
             $stm = $this->db->prepare($query);
+            $stm->bindParam(1, $status);
             if($stm->execute()){
                 // Getting the results fromm the database
                 $result = $stm->fetchAll(PDO::FETCH_OBJ);
