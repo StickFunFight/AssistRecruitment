1c164cea5b08b40d5c97e2171b7ef29c86fe82f1
StickFunFight <36295961+StickFunFight@users.noreply.github.com>
2020-05-15 00:05:42 +0200

    Update questionair.php

--- view/questionair.php
+++ view/questionair.php
@@ -11,10 +11,10 @@ require_once 'menu.php';
     <div class="container-fluid">
         <div class="row QaTopMargin">
             <div class="col-sm-6">
-                <input class="form-control form-control-lg" id="Filter" type="text" placeholder="Zoek naar een ">
+                <input class="form-control form-control-lg" id="Filter" type="text" placeholder="Zoek naar een questionair.">
             </div>
             <div class="col-sm-6">
-                <button type="button" class="btn add-container__btn ButtonRight" data-toggle="modal" data-target="#AxisAddModal"><i class="fas fa-plus-circle"></i>Axis toevoegen</button>
+                <button type="button" class="btn add-container__btn ButtonRight"><i class="fas fa-plus-circle"></i> Questionair toevoegen</button>
             </div>
         </div>
         <div>
@@ -47,7 +47,7 @@ require_once 'menu.php';
                     echo '<td>';
                     echo '<a id="'.$item->getId().'" onclick="SendID(this.id)"><i class="fas tab-table__icon editKnop">&#xf044;</i></a>';
                     echo  ' ';
-                    echo '<a id="'.$item->getId().'" onclick="SendID(this.id)"  data-id="'.$item->getId().'"><i class="fas tab-table__icon deleteKnop">&#xf187;</i></a>';
+                    echo '<a id="'.$item->getId().'" onclick="SendID(this.id)" data-id="'.$item->getId().'"><i class="fas tab-table__icon deleteKnop">&#xf187;</i></a>';
                     echo '</td>';
                     echo '</tr>';
                 }
@@ -57,3 +57,15 @@ require_once 'menu.php';
         </div>
     </div>
 </div>
+</body>
+</html>
+<script>
+    $(document).ready(function(){
+        $("#Filter").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#QaTable #RowFilter").filter(function() {
+                $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+        });
+    });
+</script>
