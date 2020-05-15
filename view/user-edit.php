aef70d19d7df098a1ed077d6dce526124af7c994
MarvinVissers <vissers2000@gmail.com>
2020-05-15 14:11:03 +0200

    knoppen weg op user-edit

--- view/user-edit.php
+++ view/user-edit.php
@@ -368,11 +368,11 @@
                                             // Checking for customer id to know where to add the new user to
                                             if ($customerID != 0) {
                                                 ?>
-                                                    <a href="user-add?customer=<?php echo $customerID; ?>" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a>
+                                                    <!-- <a href="user-add?customer=<?php //echo $customerID; ?>" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a> -->
                                                 <?php
                                             } else {
                                                 ?>
-                                                    <a href="user-add" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a>
+                                                    <!-- <a href="user-add" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a> -->
                                                 <?php
                                             }       
                                         ?>
@@ -517,11 +517,11 @@
                                             // Checking for customer id to know where to add the new user to
                                             if ($customerID != 0) {
                                                 ?>
-                                                    <a href="user-add?customer=<?php echo $customerID; ?>" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a>
+                                                    <!-- <a href="user-add?customer=<?php //echo $customerID; ?>" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a> -->
                                                 <?php
                                             } else {
                                                 ?>
-                                                    <a href="user-add" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a>
+                                                    <!-- <a href="user-add" class="btn add-container__btn"><i class='fas add-container--icon'>&#xf055;</i> Add scan</a> -->
                                                 <?php
                                             }       
                                         ?>
