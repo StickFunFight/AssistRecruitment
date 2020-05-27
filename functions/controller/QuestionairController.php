8a7747ae45c76ba3b1c2780656a8dc8d7939d8ff
Stan <stanbegthel@gmail.com>
2020-05-27 13:02:55 +0200

    -
    -

--- functions/controller/QuestionairController.php
+++ functions/controller/QuestionairController.php
@@ -2,21 +2,43 @@
 
 require '../functions/datalayer/QuestionairDatabase.php';
 
-class QuestionairController
+class questionairController
 {
+
     private $db;
+    private $QuestionairID;
 
     public function __construct()
     {
         $this->db = new QuestionairDatabase();
     }
 
-    public function GetQuestionair(){
+    public function setQuestionairID($QID)
+    {
+        $this->QuestionairID= $QID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQuestionairID()
+    {
+        return $this->QuestionairID;
+    }
+
+    public function getQuestionairList(){
         $Lijst = array();
 
-        $Lijst = $this->db->GetQuestionair();
+        $Lijst = $this->db->GetQuestionairList($this->QuestionairID);
 
         // Returning the list given from the Database class
         return $Lijst;
+    }
+
+    public function getQuestions(){
+        $lijst2 = array();
+        $lijst2 = $this->db->getQuestions();
+
+        return $lijst2;
     }
 }
